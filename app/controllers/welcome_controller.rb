fe4e10f8ff4fc7290028c76b9dada3dde31fd4ee
Saman <mohamadi.saman@gmail.com>
2015-09-11 20:57:38 +0430

    before starting angular

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,9 @@
 class WelcomeController < ApplicationController
+
+
+  def index
+    response = HTTParty.get('http://127.0.0.1/allData.json')
+    puts response.body, response.code, response.message, response.headers.inspect
+    @test = 'sdfsdf'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  get 'test', to: 'welcome#index'
   devise_for :users
   resources :users
+  resources :article
 end
